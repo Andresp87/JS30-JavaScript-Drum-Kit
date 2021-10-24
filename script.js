3dfeaad61226f43f46ff4165a8ebe6d8762bf4f3
Andres Patino <andrespa87@gmail.com>
2021-10-23 19:26:49 -0500

    sound files downloaded

---
+++ script.js
@@ -0,0 +1,34 @@
+// const A = document.getElementsByClassName("buttonA");
+
+// const soundFile = [
+//     new Audio ('clap.wav'),
+//     new Audio ('hihat.wav'),
+//     new Audio ('kick.wav'),
+//     new Audio ('openhat.wav'),
+//     new Audio ('boom.wav'),
+//     new Audio ('ride.wav'),
+//     new Audio ('snare.wav'),
+//     new Audio ('tom.wav'),
+//     new Audio ('tink.wav'),
+// ]
+
+// console.log(A);
+
+// A.addEventListener("click");
+
+const soundFile = {
+    key: 65,
+    sound: new Audio ('clap.wav'),
+}
+
+
+document.addEventListener('keydown', test);
+
+function test(event) {
+        
+        if (event.keyCode == soundFile.key) {
+            soundFile.sound.play();
+        }
+
+    console.log(event.keyCode);
+};
